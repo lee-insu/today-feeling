1cdb6070aab58786663ea6857289ec6870ad64e4
lee-insu <carpediem7760@gmail.com>
2021-02-10 18:53:21 +0900

    responsive edit

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,9 +1,10 @@
 const burger = document.querySelector(".burger"),
       nav = document.querySelector(".nav-links"),
-      navLinks = document.querySelectorAll(".nav-links li");
+      navLinks = document.querySelectorAll(".nav-links li"),
+      body = document.querySelector('body');
 
 const navAnimation = () => {
-    navLinks.forEach((link,index) => {
+    navLinks.forEach((link) => {
         if(link.style.animation) {
             link.style.animation = "";
         }else {
@@ -26,11 +27,12 @@ const setNavTransition = (width) => {
     if (width < 480) {
         nav.style.transition = "";
     }else {
-        nav.style.transition ="transform 0.5s ease-in"
+        nav.style.transition ="transform 0.5s ease-in";
+        
     }
 };
 
-const handleResize = (e) => {
+const handleResize = () => {
     const width = e.target.innerWidth;
     setNavTransition(width);
 };
