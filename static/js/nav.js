3dd9044b47a1582a3147f7554743007f472a912b
lee-insu <carpediem7760@gmail.com>
2021-02-10 17:42:31 +0900

    responsive

---
+++ static/js/nav.js
@@ -0,0 +1,43 @@
+const burger = document.querySelector(".burger"),
+      nav = document.querySelector(".nav-links"),
+      navLinks = document.querySelectorAll(".nav-links li");
+
+const navAnimation = () => {
+    navLinks.forEach((link,index) => {
+        if(link.style.animation) {
+            link.style.animation = "";
+        }else {
+            link.style.animation = `navLinkFade 0.5s ease forwards`;
+        }
+    });
+}
+
+const handleNav = () => {
+    nav.classList.toggle("nav-active");
+    navAnimation();
+    burger.classList.toggle("toggle");
+};
+
+const navSlide = () => {
+    burger.addEventListener("click",handleNav);
+};
+
+const setNavTransition = (width) => {
+    if (width < 480) {
+        nav.style.transition = "";
+    }else {
+        nav.style.transition ="transform 0.5s ease-in"
+    }
+};
+
+const handleResize = (e) => {
+    const width = e.target.innerWidth;
+    setNavTransition(width);
+};
+
+const init = () => {
+    window.addEventListener("resize",handleResize);
+    navSlide();
+};
+
+init();
